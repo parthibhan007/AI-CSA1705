1bb33f8c956d3d5903f03114297f2846019a897c
parthibhan007 <129139798+parthibhan007@users.noreply.github.com>
2023-11-30 00:54:23 -0800

    Add files via upload

---
+++ A star.py
@@ -0,0 +1,78 @@
+from collections import deque
+
+class Graph:
+    def __init__(self, adjacency_list):
+        self.adjacency_list = adjacency_list
+
+    def get_neighbors(self, v):
+        return self.adjacency_list[v]
+
+    def h(self, n):
+        H = {
+            'A': 6,
+            'B': 4,
+            'C': 5,
+            'D': 0
+        }
+        return H[n]
+
+    def a_star_algorithm(self, start_node, stop_node):
+        open_list = set([start_node])
+        closed_list = set([])
+        g = {}
+        g[start_node] = 0
+        parents = {}
+        parents[start_node] = start_node
+
+        while len(open_list) > 0:
+            n = None
+            for v in open_list:
+                if n is None or g[v] + self.h(v) < g[n] + self.h(n):
+                    n = v
+
+            if n is None:
+                print('Path does not exist!')
+                return None, None
+
+            if n == stop_node:
+                reconst_path = []
+
+                while parents[n] != n:
+                    reconst_path.append(n)
+                    n = parents[n]
+
+                reconst_path.append(start_node)
+                reconst_path.reverse()
+
+                total_cost = g[stop_node]  
+                print('Path found: {}, Cost: {}'.format(reconst_path, total_cost))
+                return reconst_path, total_cost
+
+            for (m, weight) in self.get_neighbors(n):
+                if m not in open_list and m not in closed_list:
+                    open_list.add(m)
+                    parents[m] = n
+                    g[m] = g[n] + weight
+                else:
+                    if g[m] > g[n] + weight:
+                        g[m] = g[n] + weight
+                        parents[m] = n
+
+                        if m in closed_list:
+                            closed_list.remove(m)
+                            open_list.add(m)
+
+            open_list.remove(n)
+            closed_list.add(n)
+
+        print('Path does not exist!')
+        return None, None
+
+adjacency_list = {
+    'A': [('B', 1), ('C', 3), ('D', 7)],
+    'B': [('D', 5)],
+    'C': [('D', 12)]
+}
+
+graph1 = Graph(adjacency_list)
+path, cost = graph1.a_star_algorithm('A', 'D')
